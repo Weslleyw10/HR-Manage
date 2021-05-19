2b27558f03f488af8d6775c369023e2caaa2a2be
Weslley L Silva <weslleylopes.dev@gmail.com>
2021-05-18 23:35:04 -0300

    Add employee app

---
+++ employee/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return str(self.name)
